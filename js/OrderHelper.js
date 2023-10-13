6617cb2beb8cb40d6b50a4a3c84b55befbbb27de
Mr-Kaos <mast_kaos@hotmail.com>
2023-10-13 15:18:37 +1100

    Moved function for testing to its own file

--- js/OrderHelper.js
+++ js/OrderHelper.js
@@ -61,41 +61,6 @@ function toggleProduct(productName) {
 }
 
 /**
- * Checks if the date entered is valid against the customer's delivery day offset and working day offsets
- * If it is not, a warning message is displayed.
- * @param {Element} dateField The due date input element
- * @param {Number} deliveryDayOffset The number of days in advance to deliver the order
- * @param {Number} beginWorkingOffset The number of days the order is expected to take to complete
- * @param {String} defaultDueTime The default time of day in which order is due/needs to be dispatched
- */
-function validateDueDate(dateField, deliveryDayOffset, beginWorkingOffset, defaultDueTime) {
-	let dueDate = new Date(dateField.value);
-
-	// Determine what the deadline of the job would be based on the delivery offset
-	let deadline = new Date(dateField.value);
-	deadline.setDate(dueDate.getDate());
-	deadline.setHours(defaultDueTime.split(':')[0], defaultDueTime.split(':')[1]);
-
-	while (deliveryDayOffset > 0) {
-		deadline.setDate(deadline.getDate() - 1);
-		if (!(deadline.getDay() === 0 || deadline.getDay() === 6)) {
-			deliveryDayOffset -= 1;
-		}
-	}
-
-	let difference = (deadline - Date.now()) / (1000 * 60 * 60 * 24);
-
-	console.log(difference, beginWorkingOffset);
-	// Check if the deadline can be met by the begin working day offset. If the difference is less than beginWorkingOffset, warn the user.
-	if (difference < beginWorkingOffset) {
-		appendErrorMessage(dateField, `The due date falls short of this customer's minimum required working days. You may have less time to complete this job than normal.\nRequired Despatch date: ${deadline.toLocaleString()}`, ALERT_WARN);
-	} else {
-		removeErrorMessage(dateField);
-		appendErrorMessage(dateField, `Required Despatch date:\n ${deadline.toLocaleString()}\n`, ALERT_NONE);
-	}
-}
-
-/**
  * Displays the delivery offset values for the selected customer.
  * Used for demonstration purposes.
  * @param {*} element 
@@ -112,11 +77,6 @@ function displayDeliveryOffsets(element) {
 function init() {
 	let toggleDisplayBtn = document.getElementById("toggleExtraFields");
 	let customerDropdown = document.getElementById("Customer");
-	let dueDate = document.getElementById('DateDue');
-	let deliveryOffset = document.getElementById('deliveryOffset');
-	let beginWorkOffset = document.getElementById('beginworkingoffset');
-	let dueTime = document.getElementById('defaultduetime');
-	let i = 0;
 
 	if (toggleDisplayBtn !== null) {
 		toggleDisplayBtn.addEventListener("click", toggleExtraFields);
@@ -125,14 +85,6 @@ function init() {
 	customerDropdown.addEventListener('change', e => {
 		displayDeliveryOffsets(e.target.children[customerDropdown.selectedIndex]);
 	});
-
-	if (dueDate !== null) {
-		[dueDate, deliveryOffset, beginWorkOffset, dueTime].forEach(element => {
-			element.addEventListener('change', function (e) {
-				validateDueDate(dueDate, deliveryOffset.value, beginWorkOffset.value, dueTime.value);
-			});
-		});
-	}
 }
 
 window.addEventListener("load", init());
---
+++ js/validateDueDate.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/**
+ * Checks if the date entered is valid against the customer's delivery day offset and working day offsets
+ * If it is not, a warning message is displayed.
+ * @param {Element} dateField The due date input element
+ * @param {Number} deliveryDayOffset The number of days in advance to deliver the order
+ * @param {Number} beginWorkingOffset The number of days the order is expected to take to complete
+ * @param {String} defaultDueTime The default time of day in which order is due/needs to be dispatched
+ */
+function validateDueDate(dateField, deliveryDayOffset, beginWorkingOffset, defaultDueTime) {
+	let dueDate = new Date(dateField.value);
+
+	// Determine what the deadline of the job would be based on the delivery offset
+	let deadline = new Date(dateField.value);
+	deadline.setDate(dueDate.getDate());
+	deadline.setHours(defaultDueTime.split(':')[0], defaultDueTime.split(':')[1]);
+
+	while (deliveryDayOffset > 0) {
+		deadline.setDate(deadline.getDate() - 1);
+		if (!(deadline.getDay() === 0 || deadline.getDay() === 6)) {
+			deliveryDayOffset -= 1;
+		}
+	}
+
+	let difference = (deadline - Date.now()) / (1000 * 60 * 60 * 24);
+	// Check if the deadline can be met by the begin working day offset. If the difference is less than beginWorkingOffset, warn the user.
+	if (difference < beginWorkingOffset) {
+		appendErrorMessage(dateField, `The due date falls short of this customer's minimum required working days. You may have less time to complete this job than normal.\nRequired Despatch date: ${deadline.toLocaleString()}`, ALERT_WARN);
+	} else {
+		removeErrorMessage(dateField);
+		appendErrorMessage(dateField, `Required Despatch date:\n ${deadline.toLocaleString()}\n`, ALERT_NONE);
+	}
+}
+
+function init() {
+	let dueDate = document.getElementById('DateDue');
+	let deliveryOffset = document.getElementById('deliveryOffset');
+	let beginWorkOffset = document.getElementById('beginworkingoffset');
+	let dueTime = document.getElementById('defaultduetime');
+
+	if (dueDate !== null) {
+		[dueDate, deliveryOffset, beginWorkOffset, dueTime].forEach(element => {
+			element.addEventListener('change', function (e) {
+				validateDueDate(dueDate, deliveryOffset.value, beginWorkOffset.value, dueTime.value);
+			});
+		});
+	}
+}
+
+window.addEventListener("load", init());
